c9c2079e08f7aaf7c617c944a1180976c3141212
Benjamin2404-AB <benab2404@gmail.com>
2025-05-10 02:33:17 +0100

    new fix

--- absoncoWeb/absoncoWeb/urls.py
+++ absoncoWeb/absoncoWeb/urls.py
@@ -18,6 +18,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from debug_toolbar.toolbar import debug_toolbar_urls # type: ignore
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,3 +28,6 @@ urlpatterns = [
     path('payment/',include('payment.urls',namespace='payment')),
     
 ] + debug_toolbar_urls()
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
