bde1181ded0b3ef6e43ffca140d923a8111379e7
Benjamin2404-AB <benab2404@gmail.com>
2025-05-10 02:23:51 +0100

    file

--- absoncoWeb/absoncoWeb/settings.py
+++ absoncoWeb/absoncoWeb/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['absoncoenterprise.onrender.com', 'localhost','0.0.0.0']
+ALLOWED_HOSTS = ['absoncoweb.onrender.com', 'localhost','0.0.0.0']
 
 # static files configuration
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
